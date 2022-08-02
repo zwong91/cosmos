b85fddad642d02e84944d7cb47aba8c0fe15923b
wong <yzbingchuan@gmail.com>
2022-08-02 12:04:34 +0800

    45天自制操作系统

---
+++ lesson01/HelloWorld/HelloWorld.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+// Ken Thompson Dennis Ritchie 阿兰·图灵 冯诺依曼
+//  objdump -d HelloWorld  
+// C 语言中函数的调用和返回功能，CPU 实现了函数调用和返回指令 call / ret
+// 预处理程序、编译程序、汇编程序、链接程序
+int main(int argc, char const *argv[])
+{
+    printf("hello world!\n");
+    return 0;
+}
