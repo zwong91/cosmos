046a890a8060a499e939a37910f8761e7fba72d8
wong <yzbingchuan@gmail.com>
2022-08-02 12:34:05 +0800

    hello os  1 -2

--- lesson01/HelloWorld/HelloWorld.c
+++ lesson01/HelloWorld/HelloWorld.c
@@ -1,8 +1,8 @@
 #include "stdio.h"
-// Ken Thompson Dennis Ritchie 阿兰·图灵 冯诺依曼
+// 阿兰·图灵Turing 冯诺依曼 Ken Thompson Dennis Ritchie 
 //  objdump -d HelloWorld  
 // C 语言中函数的调用和返回功能，CPU 实现了函数调用和返回指令 call / ret
-// 预处理程序、编译程序、汇编程序、链接程序
+// 预处理程序i、编译程序o、汇编程序s、链接程序ld
 int main(int argc, char const *argv[])
 {
     printf("hello world!\n");
